66fcb5489cad8330e4bc3f2f6c632f1ed61e6dc0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-03 20:54:39 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Hardcoded placeholder for what will eventually be a submitted DSL script.
+	script := "print 1 + 1"
+
+	fmt.Println("dslementary: executing hardcoded script")
+	fmt.Println("script:", script)
+	fmt.Println("result: 2")
+}
